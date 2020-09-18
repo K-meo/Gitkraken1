9679673396d0416393ca08edb93649f264588982
Flo <test@k-meo.com>
2020-09-18 11:07:48 +0200

    Modifs images

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 	echo "Hello !!";
-	echo '<img src="images/fond.jpg" />';
+	echo '<img src="images/fond.jpg" width="450" />';
 	echo '<br />';
-	echo '<img src="images/fond.jpg" />';
+	echo '<img src="images/hdr-27.jpg" width="450" />';
 ?>
