69ed510c91f7a1bc9dee5c75aba8ecd92100a8fe
Flo <test@k-meo.com>
2020-09-18 11:13:56 +0200

    Affiche 4 images

--- test.php
+++ test.php
@@ -3,4 +3,8 @@
 	echo '<img src="images/fond.jpg" width="450" />';
 	echo '<br />';
 	echo '<img src="images/hdr-27.jpg" width="450" />';
+	echo '<br />';
+	echo '<img src="images/hdr-49.jpg" width="450" />';
+	echo '<br />';
+	echo '<img src="images/hdr-102.jpg" width="450" />';
 ?>
